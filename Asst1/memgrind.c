fd102330a23eb8c47d5f7ed6c08b56cfa16ec092
dodecaphobia <cba30@scarletmail.rutgers.edu>
2016-10-14 14:07:50 -0400

    Add files via upload

--- Asst1/memgrind.c
+++ Asst1/memgrind.c
@@ -1,27 +1,40 @@
-#include <stdio.h>
-
 #include "mymalloc.h"
+#include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
 
+/*
 #define malloc( x ) mymalloc( x, __FILE__, __LINE__ )
-#define free( x ) myfree( x, __FILE__, __LINE__ ) 
+#define free( x ) myfree( x, __FILE__, __LINE__ ) */
 
 int main()
 {
-	mymallocInit();
+	double avg = 0;
+	int j = 0;
+
 	/* test 1 */
-
-	/* test 2 */
-	void * temp;
+	printf("%d     ", j);
+	avg = 0;
+	j = 0;
+	void ** temp1[3000];
 	int i = 0;
-	while(i < 3000)
-	{
-		temp = malloc(1);
-		free(temp);
-		i++;
+	while(j < 100) {
+		clock_t begin = clock();
+		i = 0;
+		while(i < 3000)
+			temp1[i] = malloc(1);
+		printf("Done malloc\n");
+		i = 0;
+		while(i < 3000)
+			free(temp1[i]);
+		clock_t end = clock();
+		avg += (double)(end - begin);
+		j++;
 	}
-	/* test 3 */
+	avg /= 100;
 
-	/* test 4 */
+	printf("Test 1 successful!\nAverage time: %d sec\n\n", avg);
+
 
 	return 0;
 }
