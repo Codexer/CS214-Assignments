5532d5e9b25ba21c614cd028298f06f8f60c7af9
dodecaphobia <cba30@scarletmail.rutgers.edu>
2016-10-14 14:20:09 -0400

    Add files via upload

--- Asst1/memgrind.c
+++ Asst1/memgrind.c
@@ -9,16 +9,18 @@
 
 int main()
 {
+	mymallocInit();
+	printf("Init complete\n");
 	double avg = 0;
 	int j = 0;
 
 	/* test 1 */
-	printf("%d     ", j);
 	avg = 0;
 	j = 0;
 	void ** temp1[3000];
 	int i = 0;
 	while(j < 100) {
+		printf("%d     ", j);
 		clock_t begin = clock();
 		i = 0;
 		while(i < 3000)
