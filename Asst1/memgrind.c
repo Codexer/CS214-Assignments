ecf2dd52d8ce36d35c253a85d625b400de5b1f8c
Matthew <mattantoy@gmail.com>
2016-10-14 18:37:17 -0400

    Fixed "mymalloc.h" and loops and printf

--- Asst1/memgrind.c
+++ Asst1/memgrind.c
@@ -1,7 +1,8 @@
-#include "mymalloc.h"
 #include <stdio.h>
 #include <time.h>
 #include <stdlib.h>
+
+#include "mymalloc.h"
 
 /*
 #define malloc( x ) mymalloc( x, __FILE__, __LINE__ )
@@ -23,19 +24,23 @@ int main()
 		printf("%d     ", j);
 		clock_t begin = clock();
 		i = 0;
-		while(i < 3000)
+		while(i < 3000) {
 			temp1[i] = malloc(1);
+			i++;
+		}
 		printf("Done malloc\n");
 		i = 0;
-		while(i < 3000)
+		while(i < 3000) {
 			free(temp1[i]);
+			i++;
+		}
 		clock_t end = clock();
 		avg += (double)(end - begin);
 		j++;
 	}
 	avg /= 100;
 
-	printf("Test 1 successful!\nAverage time: %d sec\n\n", avg);
+	printf("Test 1 successful!\nAverage time: %f sec\n\n", avg);
 
 
 	return 0;
